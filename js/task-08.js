bdca735ab724b33a37a965f69ee41a3d91a8e123
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-07-31 23:06:28 +0300

    solve task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        mail, password,
+    };
+
+    console.log(formData);
+    if (mail === '' && password === '') {
+        alert('All fields must be completed! (Усі поля мають бути заповнені!)')
+    }
+    event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const nameColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  nameColor.innerText = color;
+});
+
