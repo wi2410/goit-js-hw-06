6283ce86eaf9c9073b4fae876ae44583bb12aefe
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-01 11:05:28 +0300

    refactoring task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -6,9 +6,9 @@ function getRandomHexColor() {
 
 const container = document.querySelector('#boxes');
 const numberEl = document.querySelector("#controls input");
-const minValue = numberEl.getAttribute('min');
-const maxValue = numberEl.getAttribute('max');
-const step = numberEl.getAttribute('step');
+const minValue = +numberEl.getAttribute('min');
+const maxValue = +numberEl.getAttribute('max');
+const step = +numberEl.getAttribute('step');
 const btnCreate = document.querySelector("#controls button[data-create]");
 const btnDestroy = document.querySelector("#controls button[data-destroy]");
 
@@ -17,7 +17,7 @@ function createBoxes(amount) {
   const elements = []
   let width = 30;
   let height = 30;
-  for (let i = 0; i < amount; i+= +step) {
+  for (let i = 0; i < amount; i+= step) {
       const boxEl = document.createElement('div')
       boxEl.style.width = width + 'px';
       boxEl.style.height = height + 'px';
